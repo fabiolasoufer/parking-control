934c81c17c8348731cc14a13edf6c2129c9e3c4a
Fabiola <fabiolasou.za@live.com>
2022-04-28 11:29:08 -0300

    @fabiolasoufer [Task: Finalizando API Parking Spot Control]

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -9,4 +9,8 @@ import com.api.parkingcontrol.models.ParkingSpotModel;
 
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+	
+	boolean existsByLicensePlateCar(String licensePlateCar);
+	boolean existsByParkingSpotNumber(String parkingSpotNumber);
+	boolean existsByApartmentAndBlock(String apartment, String block);
 }
